08a59b2637fb3ae12d42fa9072169f660541ea27
ostapViniavskyi <viniavskyi@ucu.edu.ua>
2018-03-01 18:11:27 +0200

    completed Field class

--- battleship_gameplay.py
+++ battleship_gameplay.py
@@ -1,6 +1,7 @@
 from battleship_additional_functions import generate_field
 from battleship_ship import Ship
 
+
 class Field:
 
     def __init__(self):
@@ -16,5 +17,30 @@ class Field:
         """
         x, y = coords
         self.__ships[x][y].shoot_at(coords)
+        self.__ships[x][y] = 'X'
 
-    def
+    def field_without_ships(self):
+        """
+        :return: string of field with marked cells that where hit by enemy
+        """
+        bf_txt = [['X' if self.__ships[i][j] == 'X'
+                   else ' ' for i in range(10)] for j in range(10)]
+
+        first_line = '   A B C D E F G H I J\n'
+        return first_line + '\n'.join([' ' * (2 - len(str(x + 1))) +
+                                       str(x + 1) + ' ' + ' '.join(bf_txt[x])
+                                       for x in range(10)])
+
+    def field_with_ships(self):
+        """
+        :return: string of field with marked cells that where hit by enemy
+        and ships
+        """
+        bf_txt = [['X' if self.__ships[i][j] == 'X'
+                   else '■' if isinstance(self.__ships[i][j], Ship)
+                   else ' ' for i in range(10)] for j in range(10)]
+
+        first_line = '   A B C D E F G H I J\n'
+        return first_line + '\n'.join([' ' * (2 - len(str(x + 1))) +
+                                       str(x + 1) + ' ' + ' '.join(bf_txt[x])
+                                       for x in range(10)])
